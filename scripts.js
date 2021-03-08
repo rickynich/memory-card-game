c14cc1d1a1de594d5827812ee62c98657c6f11d1
Nick Richard <nrich414@gmail.com>
2021-03-08 11:07:23 -0500

    store cards complete

--- scripts.js
+++ scripts.js
@@ -1,8 +1,22 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
-    console.log("I was clicked!")
-    console.log(this)
     this.classList.toggle('flip')
+    if (!hasFlippedCard) {
+        //first click on a card
+        hasFlippedCard = true
+        firstCard = this;
+    } else {
+        hasFlippedCard = false;
+        secondCard = this;
+        // console.log(secondCard, firstCard)
+
+        //do cards match
+        console.log(firstCard.dataset.framework)
+        console.log(secondCard.dataset.framework)
+    }
 }
 cards.forEach(card => card.addEventListener('click', flipCard))
