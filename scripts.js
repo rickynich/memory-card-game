2e670593fc8def542bec5065a23e9e899dd02506
Nick Richard <nrich414@gmail.com>
2021-03-11 20:13:31 -0500

    more refactoring, error fix for card not flipping, setTimeout change

--- scripts.js
+++ scripts.js
@@ -4,17 +4,20 @@ let hasFlippedCard = false;
 let firstCard, secondCard;
 
 function flipCard() {
-	this.classList.toggle("flip");
+	this.classList.add('flip');
+
 	if (!hasFlippedCard) {
 		//first click on a card
 		hasFlippedCard = true;
 		firstCard = this;
-	} else {
-		hasFlippedCard = false;
-		secondCard = this;
-
-		checkForMatch();
+		return;
 	}
+	
+	hasFlippedCard = false;
+	secondCard = this;
+
+	checkForMatch();
+
 	function checkForMatch() {
 		let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
 
@@ -32,7 +35,7 @@ function unflipCards() {
 	setTimeout(() => {
 		firstCard.classList.remove("flip");
 		secondCard.classList.remove("flip");
-	}, 1500);
+	}, 1100);
 }
 
 cards.forEach((card) => card.addEventListener("click", flipCard));
